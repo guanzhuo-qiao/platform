f56591bb351aa19cba611bca2c8e99b1890bfe0c
Ang Li <ali24@stevens.edu>
2019-11-17 17:24:59 -0500

    group function

--- backtest_platform.py
+++ backtest_platform.py
@@ -6,14 +6,33 @@ class Platform:
     def __init__(self, data, factor=None):
         self.data = data
         self.factor = factor
+        self.quarterly_return = None
+        self.grouped_return = None
 
-    def processor(self):
+    def processor(self): # get self.quarterly_return
         pass
 
     def group(self, N, means):
-        pass
+        self.grouped_return = pd.DataFrame(columns=range(N))
+        for date, factors in self.factor.iterrows():
+            ticker_list = list(factors.sort_values().index)
+            members = int(len(ticker_list) / N)
+            gr_row = []
+            for i in range(N):
+                if i == N - 1:
+                    grouped_tickers = ticker_list[i * members:]
+                else:
+                    grouped_tickers = ticker_list[i * members:(i + 1) * members]
+                gr_row.append(self.quarterly_return.loc[date, grouped_tickers].mean())
+            self.grouped_return.append(gr_row)
 
-    def get_performance(self, factor):
-        self.factor = factor
+
+
+
+    def get_performance(self, factor=None):
+        if factor:
+            self.factor = factor
+
+
 
 
