44577334c873ec5ee8c9308ff9686e13f594f3c4
Ang Li <ali24@stevens.edu>
2019-11-17 15:03:06 -0500

    platform setup

--- backtest_platform.py
+++ backtest_platform.py
@@ -1,10 +1,18 @@
 import numpy as np
 import pandas as pd
 
+
 class Platform:
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data
+
+    def processor(self):
+        pass
+
+    def group(self, N, means):
         pass
 
     def get_performance(self, factor):
         pass
+
 
